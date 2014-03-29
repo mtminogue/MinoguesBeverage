1aae160184b5216fc41a4ce7789afa8f5583f30f
Michael T. Minogue <michael.minogue@gmail.com>
2014-03-29 10:46:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinoguesBeverage::Application.config.secret_key_base = 'f6ee59293262e0c5c107a623d7d1a0e6ef7baec67ef8504a23b13793c568df321857275d8945502b608afcfc444c74ed8e28c9183dba4e1b9ab469eb73fe3cf1'
